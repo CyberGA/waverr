ed214afe2a6a09b2894c7ddfceef8f2420cbb09b
CyberGA <etomu.joshua@gmail.com>
2021-08-03 13:07:12 +0100

    initialize the waverr project

---
+++ android/app/src/main/kotlin/com/example/waverr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.waverr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/styles/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+const Color cPrimary = Color(0xFF1DC4C4);
